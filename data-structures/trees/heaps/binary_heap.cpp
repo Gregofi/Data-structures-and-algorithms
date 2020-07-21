1918f56323ac87e2aaa2e037b7794473943ca64b
Filip Gregor <gregofi1@fit.cvut.cz>
2020-07-21 12:55:06 +0200

    Fixed bubble_up/down and added random tests

--- data-structures/trees/heaps/binary_heap.cpp
+++ data-structures/trees/heaps/binary_heap.cpp
@@ -12,7 +12,7 @@ void swap(T &x, T &y) noexcept
 class bin_heap
 {
 public:
-  bin_heap() = default;
+  bin_heap() : size(0){}
   ~bin_heap() = default;  
   void insert(int i);
   int get_min();
@@ -20,7 +20,7 @@ public:
 private:
   void bubble_up(size_t i);
   void bubble_down(size_t i);
-  static const int MAX_SIZE = 100000;
+  static const int MAX_SIZE = 1000000;
   int arr[MAX_SIZE];
   size_t size;
 };
@@ -32,14 +32,14 @@ int bin_heap::get_min()
 
 void bin_heap::insert(int i)
 {
-  arr[size] = i;
-  bubble_up(arr[size]);
   size ++;
+  arr[size] = i;
+  bubble_up(size);
 }
 
 void bin_heap::bubble_up(size_t i)
 {
-  while(i > 0)
+  while(i / 2 > 0)
   {
     if(arr[i / 2] > arr[i])
     {
@@ -53,10 +53,8 @@ void bin_heap::bubble_up(size_t i)
 
 void bin_heap::bubble_down(size_t i)
 {
-  while(true)
+  while(size >= i*2)
   {
-    if(size < i*2)
-      break;
     size_t n;
     if(size < i*2 + 1)
       n = i*2;
@@ -70,11 +68,12 @@ void bin_heap::bubble_down(size_t i)
 void bin_heap::extract_min()
 {
   swap(arr[1], arr[size]);
-  bubble_down(arr[1]);
   size --;
+  bubble_down(1);
 }
 
 /* ====================== TESTS ====================== */
+#include <random>
 
 void basic_tests()
 {
@@ -87,11 +86,43 @@ void basic_tests()
   assert(h1.get_min() == 2);
   h1.extract_min();
   assert(h1.get_min() == 3);
+  h1.extract_min();
 
+  h1.insert(4);
+  assert(h1.get_min() == 4);
+  h1.insert(27);
+  assert(h1.get_min() == 4);
+  h1.insert(3);
+  assert(h1.get_min() == 3);
+  h1.insert(1);
+  assert(h1.get_min() == 1);
 }
 
+#include <set>
+#define TEST_SIZE 500000
+void random_test()
+{
+  std::random_device rd;
+  std::mt19937 e{rd()};
+
+  std::multiset<int> ms;
+  bin_heap h;
+  for(size_t i = 0; i < TEST_SIZE; ++ i)
+  {
+    int x = e();
+    ms.insert(x);
+    h.insert(x);
+    if(i % 5 == 0)
+    {
+      assert(h.get_min() == *ms.begin());
+      h.extract_min();
+      ms.erase(ms.begin());
+    }
+  }
+}
 
 int main()
 {
-
+  basic_tests();
+  random_test();
 }
